67afda27f383833ae89da76e4b055b07f9768e42
Mohammed Musa Ibrahim <Princejnr15@gmail.com>
2022-09-13 19:47:10 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
+#include "alx.h"
+
+/**
+ * main - Prints "ALX" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('A');
+	_putchar('L');
+	_putchar('X');
+	_putchar('\n');
+
+	return (0);
+}
