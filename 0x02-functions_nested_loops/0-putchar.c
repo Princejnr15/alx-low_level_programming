e04d8f6d67794acc6befc3de88c3502a3de0ed6d
Mohammed Musa Ibrahim <Princejnr15@gmail.com>
2022-09-13 20:15:02 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - Prints "Alphabet" followed ba a new line.
+ * main - Prints "_putchar" followed ba a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('A')
-	_putchar('l')
+	_putchar('_')
 	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
 	_putchar('h')
 	_putchar('a')
-	_putchar('b')
-	_putchar('e')
-	_putchar('t')
+	_putchar('r')
 
 
 
